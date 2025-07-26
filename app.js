7bb10958c88d464aac94ff85659f7d0211f870f1
Andrio Contreras <andriocontreras263@gmail.com>
2025-07-26 18:53:23 -0500

    Agrega funcion agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = document.getElementById("listaAmigos");
+let resultadoSorteo = document.getElementById("resultado");
+let amigos = [];
+
+function agregarAmigo() {
+    let nombres = document.getElementById("amigo").value;
+    if (nombres.trim() == ""){
+        alert("Por favor, ingrese un nombre válido.");
+        return
+    }
+    amigos.push(nombres);
+    llenarHTML(amigos);
+    limpiarCaja();
+}
+
+function limpiarCaja(){
+    document.getElementById("amigo").value = "";
+}
+
+function llenarHTML(amigos){
+    listaAmigos.innerHTML = "";
+    amigos.forEach(amigo => {        
+        let lista = document.createElement('li');
+        lista.textContent = amigo;
+        listaAmigos.append(lista);
+    });
+}
+
